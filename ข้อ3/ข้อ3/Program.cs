f3579054997b08e1057b001bb33a587db5880f83
ffrsr <rossarin.869@mail.kmutt.ac.th>
2021-06-13 16:53:11 +0700

    ข้อ3

---
+++ ข้อ3/ข้อ3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ข้อ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            string inputNum;
+            
+            do
+            {
+                 inputNum = Console.ReadLine();
+                
+            } while (inputNum != "End");
+
+            string mode;
+            do
+            {
+
+                mode = Console.ReadLine();
+
+                if (mode == "FindMax")
+                {
+                   
+                }
+
+                else if (mode == "FindMin")
+                {
+
+                }
+
+                else if (mode == "FindMean")
+                {
+                    
+                    
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid mode");
+                }
+            } while (mode != "FindMax" || mode != "FindMin" || mode != "FindMean");
+
+            Console.ReadLine();
+        }
+
+    }
+}
