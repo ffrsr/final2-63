111c68be33b84f5c1209f9f23630cafd6257f982
ffrsr <rossarin.869@mail.kmutt.ac.th>
2021-06-13 16:53:20 +0700

    ข้อ4

---
+++ ข้อ4/ข้อ4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ข้อ4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                string name = Console.ReadLine();
+                int numtown = int.Parse(Console.ReadLine());
+                int town = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
