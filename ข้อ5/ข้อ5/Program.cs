93808303d00a66eaec1f317da1153f23e134f68e
ffrsr <rossarin.869@mail.kmutt.ac.th>
2021-06-13 16:53:29 +0700

    ข้อ5

---
+++ ข้อ5/ข้อ5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ข้อ5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numPocket = int.Parse(Console.ReadLine());
+            
+            for ( int i = 1; i <= numPocket; i++)
+            {
+                string nameItem = Console.ReadLine();
+                string[] newnameitem = new string[i];
+                string typeItem = Console.ReadLine();
+                string[] newtypeitem = new string[i];
+
+
+                string mode = Console.ReadLine();
+
+                if (mode == "ShowAll")
+                {
+                    for (i = 1; i <= numPocket; i++)
+                    {
+                        Console.WriteLine(newnameitem[i]);
+                        Console.WriteLine(newtypeitem[i]);
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("End");
+                }
+
+            }
+
+            
+            Console.ReadLine();
+        }
+    }
+}
