f894b1d2ac1c6056bd22c09b2dc08e1c622f3e8d
ffrsr <rossarin.869@mail.kmutt.ac.th>
2021-06-13 16:52:57 +0700

    ข้อ2

---
+++ ข้อ2/ข้อ2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ข้อ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int[] num = new int[n];
+
+            for (int i = 0; i < num.Length; i++ )
+            {
+                num[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
